153877b510e55d61b638a4d38580e90840f4469c
OSMANDO ALFONSO DE JESUS MARIN ROLDAN <106894809+Osma301@users.noreply.github.com>
2022-06-26 19:53:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click",() =>{
+    document.querySelector(".nav-menu").classList.toggle('show');
+});
+ScrollReveal().reveal('.showcase', {delay: 400});
+ScrollReveal().reveal(".showcase" ,{delay: 400});
+ScrollReveal().reveal(".Come-in" , {delay: 400});
+ScrollReveal().reveal(".primerbanner", {delay: 400});
+ScrollReveal().reveal(".segundo-banner", {delay: 400});
